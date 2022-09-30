4f13d567ac3f57234d9781560a944a0bd4d1aca5
Minerva <al_mpacod@iessierradelasvillas.org>
2022-09-30 11:09:37 +0200

    Ejercicios Secuenciales: 1,2

---
+++ Ejercicio_1.py
@@ -0,0 +1,6 @@
+#Escribir un programa que pregunte al usuario su nombre, y luego lo salude.
+
+nombre = ""
+
+nombre = input("¿Cómo te llamas?: \n")
+print("Buenos días",nombre)
---
+++ Ejercicio_2.py
@@ -0,0 +1,10 @@
+#Calcular el perímetro y área de un rectángulo dada su base y su altura.
+
+base = 0
+altura = 0
+
+base = (int) (input("¿Cuál es la base?: \n"))
+altura = (int) (input("¿Cuál es la altura?: \n"))
+
+print ("El perímetro es:", ((base*2)+(altura*2)))
+print ("El área es:", (base*altura))
