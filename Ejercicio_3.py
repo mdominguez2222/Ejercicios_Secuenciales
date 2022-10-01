ebbd9adc34a540f9359d840e87d324367ce565ec
M <al_mpacod@iessierradelasvillas.org>
2022-10-01 16:22:01 +0200

    Ejercicios Secuenciales: 3,4 (acabados)

--- Ejercicio_3.py
+++ Ejercicio_3.py
@@ -1,6 +1,12 @@
 #Dados los catetos de un triángulo rectángulo, calcular su hipotenusa.
 
+from cmath import sqrt
+
 cateto1 = 0
 cateto2 = 0
 
-cateto1 = input("¿Cuál es el cateto A?: \n")
+
+cateto1 = (int) (input("¿Cuál es el cateto A?: \n"))
+cateto2 = (int) (input("¿Cuál es el cateto B?: \n"))
+
+print ("La hipotenusa es ",sqrt(((cateto1*cateto1)+(cateto2*cateto2))))
---
+++ Ejercicio_4.py
@@ -0,0 +1,12 @@
+#Dados dos números, mostrar la suma, resta, división y multiplicación de ambos.
+
+numero1 = 0
+numero2 = 0
+
+numero1 = (int) (input ("Dime un número: \n"))
+numero2 = (int) (input ("Dime otro número: \n"))
+
+print ("La suma es:", (numero1+numero2))
+print ("La resta es:", (numero1-numero2))
+print ("La división es:", (numero1/numero2))
+print ("La multiplicación es:", (numero1*numero2))
