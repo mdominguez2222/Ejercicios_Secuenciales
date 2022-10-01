e08595938f216daffb333f53af45fb76ebaa2c8e
M <al_mpacod@iessierradelasvillas.org>
2022-10-01 16:28:50 +0200

    Ejercicios Secuenciales: 5 (acabado)

---
+++ Ejercicio_5.py
@@ -0,0 +1,8 @@
+#Escribir un programa que convierta un valor dado en grados Fahrenheit a grados Celsius.
+#Recordar que la fórmula para la conversión es: C = (F-32)*5/9
+
+grados = 0
+
+grados = (int) (input("¿Cuántos grados Fahrenheit quieres convertir?: \n"))
+
+print ("Es igual a:", ((grados-32)*5/9), "grados Celsius")
